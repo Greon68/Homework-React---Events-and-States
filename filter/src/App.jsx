2ccfda8a126dfbd4a6fa8424bebf78df76c9f253
Greon68 <greon-ya@yandex.ru>
2025-06-05 10:14:30 +0300

    initial commit

---
+++ filter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { Portfolio } from './Portfolio'
+import Menu from './Menu';
+
+function App() {
+  
+
+  return (
+    <>
+      <Menu/>
+      <Portfolio/> 
+
+    </>
+  )
+}
+
+export default App
---
+++ filter/src/Menu.jsx
@@ -0,0 +1,15 @@
+import './Menu.css'
+
+const  Menu = ()=>{
+    return (
+        <div className='menu'>
+            <button className=''>All</button>
+            <button className=''>Websites</button>
+            <button className=''>Flayers</button>
+            <button className=''>Business Cards</button>
+            
+        </div>
+    )
+}
+
+export default Menu
